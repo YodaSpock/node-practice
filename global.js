43bc32f44c1c439f99ab146a1e330e472c2900b5
Isaac Spanier <ispanier@iastate.edu>
2020-03-24 00:32:32 -0500

    basic javascript recap with node

---
+++ global.js
@@ -0,0 +1,15 @@
+console.log("Hey"); //global
+
+//setTimeout(); //call a function after a delay
+//clearTimeout(); 
+
+//setInterval(); //repetively call a function with a certain delay 
+//clearInterval();
+
+//global.console.log("Hello There")
+
+//WILL PRINT UNDEFINED because message is undefined and not global 
+///var message = ''
+//console.log(global.message)
+
+console.log(module);
